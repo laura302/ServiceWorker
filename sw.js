969de587ac7ebf25ed754d7132deff2fc0df6914
Laura <lauracontreras19102003@gmail.com>
2025-10-08 11:00:10 -0600

    Prueba4

--- sw.js
+++ sw.js
@@ -1,29 +1,27 @@
-// estructura basica de un Service Worker
+// Estructura básica de un Service Worker
 
-const { cache } = require("react");
-
-// 1. Nombre del cache y archivos a cachear
+// 1. Nombre del caché y archivos a cachear
 const CACHE_NAME = "mi-cache-v1";
 const urlsToCache = [
     "index.html",
     "offline.html"
 ];
 
-// 2. INSTALL -> se ejecuta al instlar el sw
-self.addEventListener("install", event =>{
+// 2. INSTALL -> se ejecuta al instalar el SW
+self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache=> cache.addAll(urlsToCache))
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     );
 });
 
-// 3. ACTIVATE -> se ejecuta al activarse (limpia caches viejos)
+// 3. ACTIVATE -> se ejecuta al activarse (limpia cachés viejos)
 self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(keys=>
+        caches.keys().then(keys =>
             Promise.all(
-                keys.filter(key=>key |= CACHE_NAME)
-                    .map(key => caches.dalete(key))
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
             )
         )
     );
-} );
+});
